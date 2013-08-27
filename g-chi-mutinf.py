99c4939c4d9c1411b4969a97ec3088c5b5176b15
Mohammad Muneeb Sultan <muneeb.sultan@gmail.com>
2013-08-26 18:05:27 -0700

    cleaned the code and fixed a small bug

--- g-chi-mutinf.py
+++ g-chi-mutinf.py
@@ -4,9 +4,7 @@ import numpy    # for histogram stuff
 import optparse
 import os
 import glob
-import itertools
 import random
-import pickle    # for compressing the data not really needed right now
 from collections import defaultdict
 from IPython import parallel
 import sys
@@ -22,12 +20,14 @@ def grid_writer(job,value,final_grid,average_grid):
     name2 = job[2]
     id2 = job[3]
     shuffle = job[4]
+    #if shuffle is active, then put the result in the average grid 
     if shuffle:
         average_grid[id1-1][id2-1] += value
         if id1 != id2:
             average_grid[id2-1][id1-1] += value
     else:
         final_grid[id1-1][id2-1] += value
+        #since only half the data is calculated we need to add it for the other half, the check makes sure we don't doubly add the data
         if id1 != id2:
             final_grid[id2-1][id1-1] += value
 
@@ -44,7 +44,7 @@ def job_checker(dir,jobs,final_grid,average_grid):
                for i,job in enumerate(jobs):
                 if result_dict.has_key(str(job)):
                     jobs.pop(i)
-		    value=result_dict[str(job)]
+		    value = result_dict[str(job)]
 		    grid_writer(job,float(value),final_grid,average_grid)	    
 		
 	return jobs     
@@ -126,11 +126,11 @@ def mutual_information_from_files(res_name1, res_id1, res_name2, res_id2,       
         filename_id1 = glob.glob('%s/%s???%d.xvg.h5' %(dir,res_name1,res_id1))
         filename_id2 = glob.glob('%s/%s???%d.xvg.h5' %(dir,res_name2,res_id2))
         if  filename_id1 and filename_id2:
-            f=tables.openFile(filename_id1[0])
-            dihedral_id1=numpy.array(f.root.time.grid[:])
+            f = tables.openFile(filename_id1[0])
+            dihedral_id1 = numpy.array(f.root.time.grid[:])
                 
-            f=tables.openFile(filename_id2[0])
-            dihedral_id2=numpy.array(f.root.time.grid[:])
+            f = tables.openFile(filename_id2[0])
+            dihedral_id2 = numpy.array(f.root.time.grid[:])
                 
             if shuffle:
                 dihedral_id1 = numpy.random.permutation(dihedral_id1)
@@ -140,29 +140,29 @@ def mutual_information_from_files(res_name1, res_id1, res_name2, res_id2,       
             sys.out.flush()
 	
     if test:
-		mu1=0.4
-		mu2=0.5
-		kappa1=numpy.random.random()
-		kappa2=numpy.random.random()
+		mu1 = 0.4
+		mu2 = 0.5
+		kappa1 = numpy.random.random()
+		kappa2 = numpy.random.random()
 
-		dihedral_id1=57.2957795*numpy.random.vonmises(mu1,kappa1,100000)
-		dihedral_id2=57.2957795*numpy.random.vonmises(mu2,kappa2,100000)
+		dihedral_id1 = 57.2957795*numpy.random.vonmises(mu1,kappa1,100000)
+		dihedral_id2 = 57.2957795*numpy.random.vonmises(mu2,kappa2,100000)
 
 #The magic begins here:The mutual information is defined as the 2d  sum of joint probability of x,y multiplied by the log(joint/marginal probabilites probabilities)
 
     #for windowIndex in range(1,numWin):
-    res_id1_range=[-180,180]
-    res_id2_range=[-180,180]
+    res_id1_range = [-180,180]
+    res_id2_range = [-180,180]
     
-    (joint_pdf_bin_count,id1_bin_edges,id2_bin_edges) = numpy.histogram2d(dihedral_id1[:len(dihedral_id1)],dihedral_id2,bins=bin_n,range=[res_id1_range, res_id2_range],normed=False)
-    dihedral_id1_count=numpy.sum(joint_pdf_bin_count,axis=0)
-    dihedral_id2_count=numpy.sum(joint_pdf_bin_count,axis=1)
+    (joint_pdf_bin_count,id1_bin_edges,id2_bin_edges) = numpy.histogram2d(dihedral_id1[:len(dihedral_id1)],dihedral_id2,bins=bin_n,range = [res_id1_range, res_id2_range],normed = False)
+    dihedral_id1_count = numpy.sum(joint_pdf_bin_count,axis = 0)
+    dihedral_id2_count = numpy.sum(joint_pdf_bin_count,axis = 1)
     
-    dx=id1_bin_edges[1]-id1_bin_edges[0]
-    N=len(dihedral_id1)
-    H_x=-numpy.nansum((dihedral_id1_count/N) *numpy.log(dihedral_id1_count/N))+numpy.log(dx)
-    H_y=-numpy.nansum((dihedral_id2_count/N) *numpy.log(dihedral_id2_count/N))+numpy.log(dx)
-    H_x_y=-numpy.nansum((joint_pdf_bin_count/N) *numpy.log(joint_pdf_bin_count/N))+numpy.log(dx*dx)
+    dx = id1_bin_edges[1]-id1_bin_edges[0]
+    N = len(dihedral_id1)
+    H_x = -numpy.nansum((dihedral_id1_count/N) *numpy.log(dihedral_id1_count/N))+numpy.log(dx)
+    H_y = -numpy.nansum((dihedral_id2_count/N) *numpy.log(dihedral_id2_count/N))+numpy.log(dx)
+    H_x_y = -numpy.nansum((joint_pdf_bin_count/N) *numpy.log(joint_pdf_bin_count/N))+numpy.log(dx*dx)
     
     mutual=H_x+H_y-H_x_y
 #normalizing the results
@@ -176,11 +176,11 @@ def mutual_information_from_files(res_name1, res_id1, res_name2, res_id2,       
 
     if test:
     
-        p=pearson_def(dihedral_id1,dihedral_id2)
-        mutual_a=numpy.log(numpy.sqrt(1/(1-p)))
-	H_x_a=numpy.log(2*180*sps.jn(0,kappa1))-kappa1*(sps.jn(1,kappa1)/sps.jn(0,kappa1))
-	H_y_a=numpy.log(2*180*sps.jn(0,kappa2))-kappa2*(sps.jn(1,kappa2)/sps.jn(0,kappa2))
-	H_x_y_a=(0.5*numpy.log(numpy.linalg.det(numpy.cov(dihedral_id1,dihedral_id2))*(2*numpy.pi*numpy.e)**2))
+        p = pearson_def(dihedral_id1,dihedral_id2)
+        mutual_a = numpy.log(numpy.sqrt(1/(1-p)))
+	H_x_a = numpy.log(2*180*sps.jn(0,kappa1))-kappa1*(sps.jn(1,kappa1)/sps.jn(0,kappa1))
+	H_y_a = numpy.log(2*180*sps.jn(0,kappa2))-kappa2*(sps.jn(1,kappa2)/sps.jn(0,kappa2))
+	H_x_y_a = (0.5*numpy.log(numpy.linalg.det(numpy.cov(dihedral_id1,dihedral_id2))*(2*numpy.pi*numpy.e)**2))
 		##print stuff out
 	print "\nTesting on VonMises Distribution with mu1,mu2,kappa1,kappa2",mu1,mu2,kappa1,kappa2,"with",bin_n,"bins\n"
 	print "Variable","*"*20, "Calculated","*"*20,"Analytical"
@@ -193,7 +193,7 @@ def mutual_information_from_files(res_name1, res_id1, res_name2, res_id2,       
 def main(dir,total_n_residues,n_iterations,skiprows,bin_n, test,numWin,prf):
     olderr = numpy.seterr(all='ignore') 
     #Setting up the parallel stuff 
-    client_list=parallel.Client(profile=prf)
+    client_list = parallel.Client(profile=prf)
     print "Running on:",len(client_list.ids)
     view = client_list.load_balanced_view()
     
@@ -204,10 +204,10 @@ def main(dir,total_n_residues,n_iterations,skiprows,bin_n, test,numWin,prf):
     jobs = []
     
     #dict of results
-    results_dict={}
+    results_dict = {}
     
     #time_jump for saving results in seconds
-    time_jump=1
+    time_jump = 1
 
     
     if test:
@@ -221,11 +221,16 @@ def main(dir,total_n_residues,n_iterations,skiprows,bin_n, test,numWin,prf):
         print "Creating Job List"
         file_name_list=glob.glob('%s/*.h5'%dir)
         print "Found",len(file_name_list),"files"
+        #for all possible files found
         for i,file_id1 in enumerate(file_name_list):
+        #extract the name of the dihedral 
 	    name1=re.findall("chi1|chi2|chi3|chi4|phi|psi",file_name_list[i])[0]
+	    #extract the id
             id1=int((re.findall("[A-Z]{3}\d+",file_name_list[i])[0])[3:])
+        #limit the file_list to what ever is left so that we dont overcount
             for j,file_id2 in enumerate(file_name_list[i:]):
-		name2=re.findall("chi1|chi2|chi3|chi4|phi|psi",file_name_list[j])[0]
+        #get the name and id for the second residue
+		name2=re.findall("chi1|chi2|chi3|chi4|phi|psi",file_name_list[i+j])[0]
                 id2=int((re.findall("[A-Z]{3}\d+",file_name_list[j])[0])[3:])
                 for ic in range(0,n_iterations):
                     if ic==0:
@@ -236,7 +241,7 @@ def main(dir,total_n_residues,n_iterations,skiprows,bin_n, test,numWin,prf):
     print "Created",len(jobs),"jobs"               
     print "Pruning job list"
     job_checker(dir,jobs,final_grid,average_grid)
-    if len(jobs)>0:
+    if len(jobs) > 0:
         st=time.time()
         print "Start the jobs with ", len(jobs),"jobs at",st
         result = view.map(mutual_information_from_files, *zip(*jobs),chunksize=10)
@@ -253,13 +258,13 @@ def main(dir,total_n_residues,n_iterations,skiprows,bin_n, test,numWin,prf):
             except parallel.TimeoutError:
             	pass
             #   print "Here" 
-            finished=pending.difference(client_list.outstanding)
-            pending=pending.difference(finished)
+            finished = pending.difference(client_list.outstanding)
+            pending = pending.difference(finished)
             for msg_id in finished:
-                ar=client_list.get_result(msg_id)
+                ar = client_list.get_result(msg_id)
         	for singleResult in ar.result:
         	    print singleResult
-        	    results_dict[(singleResult[0])]=singleResult[1]
+        	    results_dict[(singleResult[0])] = singleResult[1]
             writeToCSV(dir,results_dict)
     
         print "BACKED UP THE DATA at %d"%(int(time.time()))
@@ -268,9 +273,10 @@ def main(dir,total_n_residues,n_iterations,skiprows,bin_n, test,numWin,prf):
         all_mutuals = result.get()
         for i,job in enumerate(jobs):
             grid_writer(job,(all_mutuals[i])[1],final_grid,average_grid)
-            
-    final_grid=final_grid-(average_grid/n_iterations) 
-    file=open('%s/'%dir+'mut-inf-mat.txt', 'w')
+    
+    #the final result is the mutual information minus the average of the noise
+    final_grid=final_grid - (average_grid/n_iterations) 
+    file = open('%s/'%dir+'mut-inf-mat.txt', 'w')
     numpy.savetxt(file, final_grid)
 
 
@@ -293,4 +299,4 @@ def parse_commandline():
 if __name__ == "__main__":
     (options, args) = parse_commandline()
     #create_hd5files_from_xvg(options.dir,options.s)
-    main(dir=options.dir, total_n_residues=options.t,n_iterations=options.i,skiprows=options.s,bin_n=options.bin_n, test=options.test,numWin=options.numWin,prf=options.prf)
+    main(dir = options.dir, total_n_residues = options.t,n_iterations = options.i,skiprows = options.s,bin_n=options.bin_n, test = options.test,numWin = options.numWin,prf = options.prf)
